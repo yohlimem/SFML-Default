906a97579124af845fdfeaefc94c98318818281b
Yahli Mariansky <yahlimem@gmail.com>
2025-01-10 19:26:44 +0200

    Game Class

---
+++ SFMLTutorial/Game/MainFrame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace SFMLTutorial
+{
+
+
+    class MainFrame : GameLoop
+    {
+        public const uint DEFAULT_WINDOW_WIDTH = 1920;
+        public const uint DEFAULT_WINDOW_HEIGHT = 1080;
+        public MainFrame() : base(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "MAIN FRAM", Color.Black)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
