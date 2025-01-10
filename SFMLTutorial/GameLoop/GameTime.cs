fe8e6079820e54bef05b400691d6a513b4d160b0
Yahli Mariansky <yahlimem@gmail.com>
2025-01-10 19:08:09 +0200

    GamerTime Updated

--- SFMLTutorial/GameLoop/GameTime.cs
+++ SFMLTutorial/GameLoop/GameTime.cs
@@ -9,7 +9,41 @@ namespace SFMLTutorial
     public class GameTime
     {
 
+        private float _deltatTime = 0f;
+        private float _timeScale = 1f;
 
+        public float TimeScale
+        {
+            get { return _timeScale; }
+            set { _timeScale = value; }
+        }
+
+        public float DeltaTime
+        {
+            get { return _deltatTime * _timeScale; }
+            set { _deltatTime = value; }
+        }
+
+        public float DletaTimeUnscaled
+        {
+            get { return _deltatTime; }
+        }
+
+        public float TotalTimeElapsed
+        {
+            get;
+            private set;
+        }
+        public GameTime()
+        {
+
+        }
+
+        public void Update(float delatTime, float totalTimeElapsed)
+        {
+            _deltatTime = delatTime;
+            TotalTimeElapsed = totalTimeElapsed;
+        }
 
     }
 }
