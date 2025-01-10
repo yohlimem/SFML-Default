7519b80b04ee7eba19e4a1a99c03fd997bf71551
Yahli Mariansky <yahlimem@gmail.com>
2025-01-10 19:21:06 +0200

    GameLoop run Updated

--- SFMLTutorial/GameLoop/GameLoop.cs
+++ SFMLTutorial/GameLoop/GameLoop.cs
@@ -29,7 +29,51 @@ namespace SFMLTutorial
         {
             BackgroundColor = backgroundColor;
             Window = new RenderWindow(new VideoMode(width, height), windowTitle);
+            this.GameTime = new GameTime();
         }
+
+        public void Run()
+        {
+            LoadContent();
+            Initialize();
+
+            float totalTimeBeforeUpdate = 0d;
+            float previousTimeElapsed = 0f;
+            float deltaTime = 0f;
+            float totalTimeElapsed = 0f;
+
+            Clock clock = new Clock;
+
+            while (Window.IsOpen)
+            {
+                Window.DispatchEvents();
+
+                totalTimeElapsed = clock.ElapsedTime.AsSeconds();
+                deltaTime = totalTimeElapsed - previousTimeElapsed;
+                previousTimeElapsed = totalTimeElapsed;
+
+                totalTimeBeforeUpdate += deltaTime;
+
+                if (totalTimeBeforeUpdate >= TIME_UNTIL_UPDATE)
+                {
+                    GameTime.Update(totalTimeBeforeUpdate, clock.ElapsedTime.AsSeconds());
+
+                    totalTimeBeforeUpdate = 0;
+
+                    Update(GameTime);
+
+                    Window.Clear(BackgroundColor);
+
+                    Draw(GameTime);
+                    Window.Display();
+                }
+            }
+        }
+
+        public abstract void LoadContent();
+        public abstract void Initialize();
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
 
     }
 }
