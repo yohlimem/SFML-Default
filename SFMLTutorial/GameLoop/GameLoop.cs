ef1f9be01bc383d2e876951715cf0b0b9a74ddc9
Yahli Mariansky <yahlimem@gmail.com>
2025-01-10 18:57:56 +0200

    initial commit

---
+++ SFMLTutorial/GameLoop/GameLoop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace SFMLTutorial
+{
+    public abstract class GameLoop
+    {
+
+        public const int TARGET_FPS = 60;
+        public const float TIME_UNTIL_UPDATE = 1f / TARGET_FPS;
+
+        public RenderWindow Window
+        {
+            get;
+            protected set;
+        }
+
+        public GameTime GameTime
+        {
+            get;
+            protected set;
+        }
+
+        public Color BackgroundColor{ get; set; }
+
+        protected GameLoop(uint width, uint height, string windowTitle, Color backgroundColor)
+        {
+            BackgroundColor = backgroundColor;
+            Window = new RenderWindow(new VideoMode(width, height), windowTitle);
+        }
+
+    }
+}
---
+++ SFMLTutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SFMLTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
