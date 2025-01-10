67ae81f8f46673771aa9154d227a8eb6b9e4f8e6
Yahli Mariansky <yahlimem@gmail.com>
2025-01-10 18:59:00 +0200

    GamerTime

--- SFMLTutorial/obj/Debug/net8.0/SFMLTutorial.AssemblyInfo.cs
+++ SFMLTutorial/obj/Debug/net8.0/SFMLTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SFMLTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0377a37e4fa258ec6c3795b0fe5f6b459b89b346")]
 [assembly: System.Reflection.AssemblyProductAttribute("SFMLTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SFMLTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
